f58d5858a88eef7cb414ce1665a4a27db6e15bfb
andresmgm0 <andresmgm0@gmail.com>
2025-08-27 20:17:33 -0300

    Fix

--- js/products.js
+++ js/products.js
@@ -1,6 +1,5 @@
 let productsArray = [];
 
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function(e){
     const url = `${PRODUCTS_URL}${101}.json`;
     fetch(url).then(res => res.json())
@@ -10,32 +9,14 @@ document.addEventListener("DOMContentLoaded", function(e){
         console.log(productsArray);
 
         showProducts(productsArray);
-=======
-document.addEventListener("DOMContentLoaded", function (e) {
-  const url = `${PRODUCTS_URL}${101}.json`;
-  fetch(url).then(res => res.json())
-    .then(data => {
-      productsArray = data.products;
-
-      console.log(productsArray);
-
-      showProducts(productsArray);
->>>>>>> Facundo
     })
     .catch(error => console.error(error));
 });
 
-<<<<<<< HEAD
 function showProducts(array){
     let contenido = ""
     array.forEach(prod => {
         contenido +=`<div class="product-card">
-=======
-function showProducts(array) {
-  let contenido = ""
-  array.forEach(prod => {
-    contenido += `<div class="product-card">
->>>>>>> Facundo
                         <img src="${prod.image}" alt="${prod.name} ">
                         <div class="product-info">
                             <h3>${prod.name}</h3>
@@ -45,11 +26,6 @@ function showProducts(array) {
                         </div>
                     </div>
                     `
-<<<<<<< HEAD
     });
     document.getElementById("productsList").innerHTML = contenido;
-=======
-  });
-  document.getElementById("productsList").innerHTML = contenido;
->>>>>>> Facundo
 };
