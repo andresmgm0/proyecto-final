34e7fddc7fc53849b904213b338b35135545f51f
Facundo de leon <64671402+fdLeon-dev@users.noreply.github.com>
2025-08-27 16:07:52 -0300

    Agregue nombre de usuario en navbar

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
-function login(){
+function login() {
     const usuer = document.getElementById("usuario").value.trim();
     const pass = document.getElementById("contrasena").value.trim();
-    if(usuer != "" && pass != ""){
+
+    if (usuer !== "" && pass !== "") {
         sessionStorage.setItem("logged", true);
+        sessionStorage.setItem("username", usuer); 
         window.location.href = "index.html";
-    } else{
+    } else {
         alert("Ingrese los datos correctamente.");
     }
 };
